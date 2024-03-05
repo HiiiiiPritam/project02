5af04e9964f964bcf84f7078cbd2559668249ecb
Pritam Roy <ratsdust4226@gmail.com>
2024-03-05 21:29:42 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,21 @@ app.use("/api/user",router)
 app.use("/api/blog",blogrouter);
 app.use("/api/category",categoryRoutes)
 
+const connectionString = 'mongodb+srv://ratsdust4226:BYZaTL6qag4ZykZq@cluster0.0divulq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => {
+      console.log("Server is running at the port 5000");
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
-mongoose.connect("mongodb+srv://ratsdust4226:BYZaTL6qag4ZykZq@cluster0.0divulq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(console.log("MongoDb connected"))
-.catch((err)=>console.log(err))
-app.listen(5000, ()=>{
-  console.log("server is running at the port 5000");
-})
+// mongoose.connect("mongodb+srv://ratsdust4226:BYZaTL6qag4ZykZq@cluster0.0divulq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(console.log("MongoDb connected"))
+// .catch((err)=>console.log(err))
+// app.listen(5000, ()=>{
+//   console.log("server is running at the port 5000");
+// })
